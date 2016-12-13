7f21b97a6e60886fb15b6f16969fa1ebeb3ccfa7
blues <blues@blues.com>
2016-12-13 16:03:44 +0800

    json data added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,49 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var Resume = {
+ 	"name": "MoMo",
+ 	"role": "Full Stack Developer",
+ 	"contact": {
+ 		"mobile": "12345678",
+ 		"email": "250299430@qq.com",
+ 		"GitHub": "https://github.com/PoBlue",
+ 		"blog": "http://www.jianshu.com/users/81f7dcc6a98e/latest_articles",
+ 		"location": "广东"
+ 	},
+ 	"bioPic": "images/portfolio.jpg",
+ 	"welcome-message": "Hello, this is my resume",
+ 	"skills": ["Android Develop", "iOS Develop", "Front-End Develop", "English", "Chinese"],
+ 	"works": [
+	 	{
+	 		"employer": "Developer",
+	 		"title": "abcCompany",
+	 		"date": "2015-10-13",
+	 		"location": "China",
+	 		"description": "iOS Develop"
+	 	}
+ 	],
+ 	"projects": [
+	 	{
+	 		"title": "Online Resume",
+	 		"date": "2016-12-13",
+	 		"description": "A Resume website",
+	 		"images": "images/comingsoon.jpg"
+	 	},
+	 	{
+	 		"title": "Portfolio Site",
+	 		"date": "2016-12-10",
+	 		"description": "A Portfolio Site",
+	 		"images": "images/comingsoon.jpg"
+	 	}
+ 	],
+ 	"online-classes": [
+ 		{
+ 			"title": "Front-End",
+ 			"school": "Udacity",
+ 			"date": "2016-12-01",
+ 			"url": "http://udacity.com"
+ 		}
+ 	]
+ };
